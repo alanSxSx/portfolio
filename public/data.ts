914d20524644f7baaf961b9492ba2ad5781ddd29
Alan Barbosa <alanzinhob@hotmail.com>
2024-01-05 12:14:55 -0300

    05_01

--- public/data.ts
+++ public/data.ts
@@ -24,7 +24,7 @@ export const dataAbout = [
     
     {
       title: "Registro de Almoço",
-      paragraph: "Aplicação utilizara pra todos os funcionários realizarem o registro de almoço diário",
+      paragraph: "Aplicação utilizada pra todos os funcionários realizarem o registro de almoço diário",
       arraySkills: ["ReactJS", "Axios","Prime React","Token JWT","Secure Local Storage","Clound Computing"],
       image: "./registro_almoco/reg1.jpeg",
       link: "https://konkero.com.br/",
@@ -43,49 +43,29 @@ export const dataAbout = [
         ],
       },
     },
+
     {
-      title: "Blog FinanZero",
-      paragraph:
-        "Migração do blog WordPress da FinanZero para NextJS. Criado template AMP no NextJS, desenvolvido um revalidador de cache no CloudFront/NextJS, para atualizar o cache após a publicação de um novo post no WordPress, implementação de um interpretador de blocos Gutenberg para componentes React.",
-      arraySkills: ["NextJS", "ReactJS", "Typescript", "Apollo", "GraphQL", "Wordpress", "SASS", "AMP"],
-      image: "./finanzero-blog-1.png",
-      link: "https://finanzero.com.br/blog/",
+      title: "Pomodoro App",
+      paragraph: "Pomodoro é um método de gerenciamento de tempo. A técnica consiste na utilização de um cronômetro para dividir o trabalho em períodos de 25 minutos, separados por breves intervalos.",
+      arraySkills: ["ReactJS", "NextJS","TypeScript","CSS","JavaScript","HTML"],
+      image: "./registro_almoco/reg1.jpeg",
+      link: "https://konkero.com.br/",
       sizeImg: {
         width: 668,
-        height: 490,
+        height: 457,
       },
       aspectRatio: "5/3",
       moreDetails: {
-        image: "./finanzero-blog-2.png",
-        titles: ["Problema a se resolver", "Minha atuação", "Benefícios da implementação"],
+        image: "./registro_almoco/reg2.jpeg",
+        titles: ["Objetivo", "Minha atuação", "Benefícios da implementação"],
         texts: [
-          "O blog da FinanZero dependia do Wordpress para seu carregamento, com isso, o SEO e performance estavam sendo penalizados.",
-          "Criação de componentes ReactJS, paginas server side, queries GraphQL e Sintetizador de voz para a leitura dos textos, como forma de acessibilidade.",
-          "Aumento no ranqueamento dos posts, maior acesso ao blog pelo público alvo e a possibilidade de publicação de Stories",
+          "O registro de almoço éra feito em uma lista de papel totalmente de forma manual, gerando muito trabalho.",
+          "Fiz toda a aplicação desde o front-end até o backend. A implementação do backend foi feita em Clound na Digital Ocean.",
+          "Reduziu drasticamente o tempo de conferência dos almoços. A empresa também terá os dados referentes ao registro de almoço para os funcionários.",
         ],
       },
     },
-    {
-      title: "Plataforma FinanZero",
-      paragraph:
-        "Atuação com a plataforma de marketplace de empréstimos, desenvolvendo mecanismos dinâmicos, estruturas de metadados, back for front, formulários, entre outros. Ambiente contande projetos React Hooks e React Class Components, NodeJS e GoLang",
-      arraySkills: ["ReactJS", "Redux", "Stylus / SASS", "Javascript", "PWA", "Webpack", "Metadados", "API Rest"],
-      image: ["./finanzero/finanzero-1.png", "./finanzero/finanzero-2.png", "./finanzero/finanzero-3.png", "./finanzero/finanzero-4.png"],
-      link: "https://finanzero.com.br/me/",
-      sizeImg: {
-        width: 545,
-        height: 841,
-      },
-      aspectRatio: "9/10",
-      moreDetails: {
-        image: ["./finanzero/finanzero-5.png", "./finanzero/finanzero-6.png", "./finanzero/finanzero-7.png", "./finanzero/finanzero-8.png"],
-        titles: ["Minha atuação", "O que trouxe de novo?"],
-        texts: [
-          "Desenvolvimento de features correspondentes as novas regras de negócios trazidas pelo Product Owner. O time era responsável por 5 projetos",
-          "Desenvolvimento de um Service Worker para gerenciamento de cache e atualização imediata dos scripts do SPA, no browser do usuário, a cada novo release ou hotfix da aplicação",
-        ],
-      },
-    },
+  
   ];
   
  
